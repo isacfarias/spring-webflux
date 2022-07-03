628649ef0fe8ecaba48de93f31474b87e746ea1f
isacfarias <isac.farias.21@gmail.com>
2022-07-03 15:19:06 -0400

    customize predicates path

--- webflux-demo/src/main/java/com/course/webfluxdemo/config/RouterConfig.java
+++ webflux-demo/src/main/java/com/course/webfluxdemo/config/RouterConfig.java
@@ -5,10 +5,7 @@ import com.course.webfluxdemo.exceptions.InputValidationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerRequest;
-import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.function.server.*;
 import reactor.core.publisher.Mono;
 
 import java.util.function.BiFunction;
@@ -28,7 +25,8 @@ public class RouterConfig {
 
     private RouterFunction<ServerResponse> serverResponseRouterFunction() {
         return RouterFunctions.route()
-                .GET("square/{input}", requestHandler::squareHandler)
+                .GET("square/{input}", RequestPredicates.path("*/1?"), requestHandler::squareHandler)
+                .GET("square/{input}", request -> ServerResponse.badRequest().bodyValue("only 10-19 allowed"))
                 .GET("table/{input}", requestHandler::tableHandler)
                 .GET("square/{input}/validation", requestHandler::squareHandlerWithValidation)
                 .GET("table/{input}/stream", requestHandler::tableStreamHandler)
