826790891990ad81096c6d900c8b25f63ffec667
isacfarias <isac.farias.21@gmail.com>
2022-07-02 21:50:26 -0400

    implementado metodos para testar captura de erros

---
+++ webflux-demo/src/main/java/com/course/webfluxdemo/controller/ReactiveMathValidationController.java
@@ -0,0 +1,40 @@
+package com.course.webfluxdemo.controller;
+
+import com.course.webfluxdemo.dto.MultiplyRequestDto;
+import com.course.webfluxdemo.dto.Response;
+import com.course.webfluxdemo.exceptions.InputValidationException;
+import com.course.webfluxdemo.services.ReactiveMathService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("reactive-math")
+@RequiredArgsConstructor
+public class ReactiveMathValidationController {
+
+    private final ReactiveMathService service;
+
+    @GetMapping("square/{input}/throw")
+    public Mono<Response> findSquere(@PathVariable int input) {
+        if ( input < 10 || input > 20 ) throw new InputValidationException(input);
+        return this.service.findSquare(input);
+    }
+
+    @GetMapping("square/{input}/mono-error")
+    public Mono<Response> monoError(@PathVariable int input) {
+        return Mono.just(input)
+                .handle((integer, sink) -> {
+                    if ( input >= 10 && input <= 20 )
+                        sink.next(integer);
+                    else
+                        sink.error(new InputValidationException(input));
+
+                })
+                .cast(Integer.class)
+                .flatMap( i -> this.service.findSquare(input));
+    }
+
+}
