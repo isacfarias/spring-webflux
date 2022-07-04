c24b8a914b3ffd169ec52dcf38b5455afd2abadb
isacfarias <isac.farias.21@gmail.com>
2022-07-04 09:07:12 -0400

    add test com stepverifier expectNextMatches

--- webflux-demo/src/test/java/com/course/webfluxdemo/Lec01GetSingleResponseTest.java
+++ webflux-demo/src/test/java/com/course/webfluxdemo/Lec01GetSingleResponseTest.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.test.StepVerifier;
 
 @Slf4j
 class Lec01GetSingleResponseTest extends BaseTest {
@@ -14,7 +15,7 @@ class Lec01GetSingleResponseTest extends BaseTest {
 
     @Test
     void blockTest() {
-        var response = this.webClient
+        final var response = this.webClient
                 .get()
                 .uri("reactive-math/square/{input}", 5)
                 .retrieve()
@@ -25,5 +26,19 @@ class Lec01GetSingleResponseTest extends BaseTest {
         log.info(response.toString());
     }
 
+    @Test
+    void stepVerifierTest() {
+       final var response = this.webClient
+                .get()
+                .uri("reactive-math/square/{input}", 5)
+                .retrieve()
+                .bodyToMono(Response.class)
+                ;
+
+        StepVerifier.create(response)
+                .expectNextMatches(payload -> payload.getOutput() == 25)
+                .expectComplete();
+
+    }
 
 }
