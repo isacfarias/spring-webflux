9446addecf82c89f854b250f110486aecebf0254
isacfarias <isac.farias.21@gmail.com>
2022-07-04 14:10:56 -0400

    add test webclient exception

---
+++ webflux-demo/src/test/java/com/course/webfluxdemo/Lec05BadRequestTest.java
@@ -0,0 +1,31 @@
+package com.course.webfluxdemo;
+
+import com.course.webfluxdemo.dto.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.test.StepVerifier;
+
+@Slf4j
+class Lec05BadRequestTest extends BaseTest {
+
+    @Autowired
+    private WebClient webClient;
+
+    @Test
+    void badRequest() {
+        final var response = this.webClient
+                .get()
+                .uri("reactive-math/square/{input}throw", 5)
+                .retrieve()
+                .bodyToMono(Response.class)
+                .doOnNext(msg -> log.info(msg.toString()))
+                .doOnError(error -> log.error(error.getMessage()));
+
+        StepVerifier.create(response)
+                    .verifyError(WebClientResponseException.BadRequest.class);
+    }
+
+}
