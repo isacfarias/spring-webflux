2de494f86d4f82a50bc2a65851bf5baa0098bf29
isacfarias <isac.farias.21@gmail.com>
2022-07-04 10:41:52 -0400

    add test webclient post

---
+++ webflux-demo/src/test/java/com/course/webfluxdemo/Lec03PostRequestTest.java
@@ -0,0 +1,40 @@
+package com.course.webfluxdemo;
+
+import com.course.webfluxdemo.dto.MultiplyRequestDto;
+import com.course.webfluxdemo.dto.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.test.StepVerifier;
+
+@Slf4j
+class Lec03PostRequestTest extends BaseTest {
+
+    @Autowired
+    private WebClient webClient;
+
+    @Test
+    void post() {
+        final var response = this.webClient
+                .post()
+                .uri("reactive-math/multiply")
+                .bodyValue(buildRequestDto(2, 5))
+                .retrieve()
+                .bodyToMono(Response.class)
+                .doOnNext(System.out::println);
+
+        StepVerifier.create(response)
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+
+    private MultiplyRequestDto buildRequestDto(int firstNumber, int secondNumber) {
+        MultiplyRequestDto dto = new MultiplyRequestDto();
+        dto.setFirst(firstNumber);
+        dto.setSecond(secondNumber);
+        return dto;
+    }
+
+
+}
