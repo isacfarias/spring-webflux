25a042007e8c2ae0c2578bf3ce31ca6a31ddc71f
isacfarias <isac.farias.21@gmail.com>
2022-07-07 14:29:06 -0400

    add dtos, entity, enums, repository

---
+++ user-service/src/main/java/com/course/userservice/enuns/TransactionStatus.java
@@ -0,0 +1,5 @@
+package com.course.userservice.enuns;
+
+public enum TransactionStatus {
+    APPROVED, DECLINED;
+}
---
+++ user-service/src/main/java/com/course/userservice/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.course.userservice.repository;
+
+import com.course.userservice.entity.User;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends ReactiveCrudRepository<User, Integer> {
+}
