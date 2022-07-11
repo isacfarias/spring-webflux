a2c5237c05af7177825e9df9126dec48aaaca0d7
isacfarias <isac.farias.21@gmail.com>
2022-07-11 19:54:50 -0400

    add dto, entity, repository

---
+++ order-service/src/main/java/com/course/orderservice/repository/PurchaseOrderRepository.java
@@ -0,0 +1,11 @@
+package com.course.orderservice.repository;
+
+
+import com.course.orderservice.entity.PurchaseOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrder, Integer> {
+
+}
