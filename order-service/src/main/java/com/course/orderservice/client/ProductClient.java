97b774dadd1902840f8f1c873f22497af83c26a3
isacfarias <isac.farias.21@gmail.com>
2022-07-11 20:12:22 -0400

    add productClient

---
+++ order-service/src/main/java/com/course/orderservice/client/ProductClient.java
@@ -0,0 +1,31 @@
+package com.course.orderservice.client;
+
+import com.course.orderservice.dto.ProductDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class ProductClient {
+
+    private final WebClient webClient;
+
+    public ProductClient(@Value("${http://localhost:8091/product/}") String url) {
+        this.webClient = WebClient
+                .builder()
+                .baseUrl(url)
+                .build();
+    }
+
+    public Mono<ProductDto> getProductById(final String productId) {
+        return this.webClient
+                .get()
+                .uri("{id}")
+                .retrieve()
+                .bodyToMono(ProductDto.class);
+    }
+
+}
