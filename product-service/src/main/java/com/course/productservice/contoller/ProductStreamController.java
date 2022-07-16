a25e57b013ada21404efe7e0a095be329ff04155
isacfarias <isac.farias.21@gmail.com>
2022-07-16 17:07:31 -0400

    correção de erro nos script js, criado filtro para stream

--- product-service/src/main/java/com/course/productservice/contoller/ProductStreamController.java
+++ product-service/src/main/java/com/course/productservice/contoller/ProductStreamController.java
@@ -5,6 +5,7 @@ import com.course.productservice.dto.ProductDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -16,9 +17,9 @@ public class ProductStreamController {
 
     private final Flux<ProductDto> productFlux;
 
-    @GetMapping(value = "stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<ProductDto> getProductUpdate() {
-        return this.productFlux;
+    @GetMapping(value = "stream/{maxPrice}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ProductDto> getProductUpdate(@PathVariable Integer maxPrice) {
+        return this.productFlux.filter(productDto -> productDto.getPrice() <= maxPrice);
     }
 
 
