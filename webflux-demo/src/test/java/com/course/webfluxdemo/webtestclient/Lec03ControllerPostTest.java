32a55d3e4e2207cd5c707aec36800b519f538103
isacfarias <isac.farias.21@gmail.com>
2022-07-17 17:15:45 -0400

    add test controllerAdvice

--- webflux-demo/src/test/java/com/course/webfluxdemo/webtestclient/Lec03ControllerPostTest.java
+++ webflux-demo/src/test/java/com/course/webfluxdemo/webtestclient/Lec03ControllerPostTest.java
@@ -40,8 +40,7 @@ public class Lec03ControllerPostTest {
                 })
                 .bodyValue(new MultiplyRequestDto())
                 .exchange()
-                .expectStatus().is2xxSuccessful()
-        ;
+                .expectStatus().is2xxSuccessful();
 
     }
 
